a20af0777d14cb794ffe33895e62ad1eaf365070
cdl-michaelyan <michael.yan@compass-canada.com>
2016-06-24 14:18:01 -0400

    add theme

--- code.R
+++ code.R
@@ -7,6 +7,35 @@ rm(list=ls())
 rawdata <- read.csv("TrainingData_Sample2.csv", stringsAsFactors=FALSE)
 
 
+library(grid)
+theme_custom <- function(base_size = 13, base_family = "Helvetica") {
+  theme_grey(base_size = base_size, base_family = base_family) %+replace%
+    theme(
+      line =              element_line(colour = '#DADADA', size = 0.75, 
+                                       linetype = 1, lineend = "butt"),
+      rect =              element_rect(fill = "#F0F0F0", colour = "#F0F0F0", 
+                                       size = 0.5, linetype = 1),
+      text =              element_text(family = base_family, face = "plain",
+                                       colour = "#656565", size = base_size,
+                                       hjust = 0.5, vjust = 0.5, angle = 0, 
+                                       lineheight = 0.9,margin = margin(), debug = FALSE),
+      
+      plot.title =        element_text(size = rel(1.5), family = '' , 
+                                       face = 'bold', hjust = -0.05, 
+                                       vjust = 1.5, colour = '#3B3B3B'),
+      axis.title.x =      element_blank(),
+      axis.title.y =      element_blank(),
+      axis.text =         element_text(),
+      axis.ticks =        element_line(),
+      panel.grid.major =  element_line(),
+      panel.grid.minor =  element_blank(),
+      plot.background =   element_rect(),
+      panel.background =  element_rect(),
+      legend.key =        element_rect(colour = '#DADADA'),
+      legend.position = 'none',
+      complete = TRUE
+    )
+}
 # discriptive -------------------------------------------------------------
 mycolor <- c(rep(c("#FF5A5F","#FFB400", "#007A87", "#8CE071", "#7B0051", "#00D1C1",
                     "#FFAA91", "#B4A76C", "#9CA299", "#565A5C", "#00A04B","#E54C20"), 100))
@@ -22,39 +51,47 @@ goaltime <- rawdata %>%
 ggplot(goaltime, aes(Minute)) + 
   geom_histogram(aes(y = ..density..),binwidth = 1,fill = mycolor[1], color = mycolor[2]) +
   geom_density(color=mycolor[3],size = 1.5) +
-    ggtitle("Histogram of Goals")
+    ggtitle("Histogram of Goals") + 
+  theme_custom()
 
 
 ggplot(rawdata %>% group_by(ID) %>% summarise(home = max(MeanH), away = max(MeanA)), aes(home)) + 
   geom_histogram(aes(y = ..density..),binwidth = 0.1,fill = mycolor[1], color = mycolor[2]) +
   geom_density(color=mycolor[3],size = 1) +
-  ggtitle("Histogram of MeanH")
+  ggtitle("Histogram of MeanH") + 
+  theme_custom()
 
 ggplot(rawdata %>% group_by(ID) %>% summarise(home = max(MeanH), away = max(MeanA)), aes(away)) + 
   geom_histogram(aes(y = ..density..),binwidth = 0.1,fill = mycolor[1], color = mycolor[2]) +
   geom_density(color=mycolor[3],size = 1) +
-  ggtitle("Histogram of MeanA")
+  ggtitle("Histogram of MeanA") + 
+  theme_custom()
 
 
-ggplot(rawdata, aes(DrawProb)) + 
-  geom_histogram(aes(y = ..density..),binwidth = 0.01,fill = mycolor[1], color = mycolor[2]) +
-  geom_density(color=mycolor[3],size = 1) +
-  facet_wrap( ~ LeagueRanking)
+ggplot(rawdata %>% group_by(ID) %>% summarise(home = max(rbS.FinalScoreH), away = max(rbS.FinalScoreA)), aes(home)) + 
+  geom_histogram(aes(y = ..density..),binwidth = 1,fill = mycolor[1], color = mycolor[2]) +
+  ggtitle("Histogram of number of HGoals") + 
+  theme_custom()
+
+ggplot(rawdata %>% group_by(ID) %>% summarise(home = max(rbS.FinalScoreH), away = max(rbS.FinalScoreA)), aes(away)) + 
+  geom_histogram(aes(y = ..density..),binwidth = 1,fill = mycolor[1], color = mycolor[7]) +
+  ggtitle("Histogram of number of AGoals") + 
+  theme_custom()
+
+
 
 ggplot(rawdata, aes(x = DrawProb, y = LeagueRanking)) +
   geom_point() +
   theme(axis.text.x = element_text(angle = 90))  + 
-  coord_flip()
-
-
-
-
+  coord_flip() + 
+  theme_custom()
 
 
 ggplot(rawdata, aes(x = ScoreDiff, y = RCDiff)) +
   geom_point() +
   theme(axis.text.x = element_text(angle = 90))  + 
-  coord_flip()
+  coord_flip()+ 
+  theme_custom()
 
 
 cleandata <- rawdata %>%
